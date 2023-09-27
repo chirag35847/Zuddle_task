b37a34f43126173dac306dfb0220822b2e92fb82
chirag <chirag.workid@gmail.com>
2023-09-28 04:17:30 +0530

    made responsive

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="h-screen w-screen bg-gradient-to-r from-cyan-500 to-blue-500 p-3">
       <h2 className="text-[2rem]">Some Title</h2>
       <SimpleGrid
-        cols={4}
+        cols={{sm: 2, lg: 4,md:4,xl:4,xs:2}}
       >
         <ResourcesCard/>
         <ToDoCard/>
